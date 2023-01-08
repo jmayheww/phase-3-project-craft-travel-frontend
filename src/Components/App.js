77d8e1f6d5a19b77e2314a60424518f44f8a21bc
Joshua <joshsmayhew@gmail.com>
2023-01-08 16:24:22 +0700

    create async function to get trip data return as json object, update trips state with retrieved trips json object data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import TripPage from "./TripPage";
+
+const tripsUrl = "http://localhost:9292/trips";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch(tripsUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setTrips(data);
+      });
+  }, []);
+
   return (
     <main className={isDarkMode ? "dark-mode" : ""}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={setIsDarkMode} />
+      <TripPage allTrips={trips} />
     </main>
   );
 }
