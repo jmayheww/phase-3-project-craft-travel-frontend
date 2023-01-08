84193874f639e8cdae5c974e194a4653b0022497
Joshua <joshsmayhew@gmail.com>
2023-01-08 12:42:40 +0700

    add body and dark mode css to index.css, import useState and useEffect hooks in App.js component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="landing">
+      <h2>Test</h2>
     </div>
   );
 }
