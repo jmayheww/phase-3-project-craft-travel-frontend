a1abed2ea7a790af5548230bf160851121819ae5
Joshua <joshsmayhew@gmail.com>
2023-01-08 15:55:00 +0700

    create TripPage component

---
+++ src/Components/TripPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TripPage() {
+  return (
+    <>
+      <div className="trips-container"></div>
+      <div className="details-container"></div>
+    </>
+  );
+}
+
+export default TripPage;
